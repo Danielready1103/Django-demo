ff268700f760255853e45a7adf7e0ff7886e2d80
Daniel <base30904@gmail.com>
2024-03-14 20:45:43 +0800

    dgango 0314 v3

--- djangodemo/urls.py
+++ djangodemo/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from app import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("index/", views.index),
+    path("", views.index),
+    path("stocks/", views.get_Stock),
 ]
