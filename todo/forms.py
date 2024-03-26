ee9629dabb62d08970ebb28c860aebd48d00d083
Daniel <base30904@gmail.com>
2024-03-26 19:45:22 +0800

    0326 Django 完成create todo

---
+++ todo/forms.py
@@ -0,0 +1,9 @@
+from django.forms import ModelForm
+from .models import Todo
+
+
+class TodoForm(ModelForm):
+    # 表單綁定資料模型
+    class Meta:
+        model = Todo
+        fields = ["title", "text", "important"]
--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
+from .forms import TodoForm
 
 # Create your views here.
 
 
-def todo(request):
+def create_todo(request):
+    form = TodoForm()
+    message = ""
+
+    if request.method == "POST":
+        try:
+            form = TodoForm(request.POST)
+            new_todo = form.save(commit=False)
+            new_todo.user = request.user
+            new_todo.save()
+            message = "建立事項成功。"
+
+            return redirect("todolist")
+        except Exception as e:
+            print(e)
+            message = "建立事項失敗..."
+
+    return render(request, "todo/create-todo.html", {"form": form, "message": message})
+
+
+def todolist(request):
     todos = None
     if request.user.is_authenticated:
         # all, get, filter
